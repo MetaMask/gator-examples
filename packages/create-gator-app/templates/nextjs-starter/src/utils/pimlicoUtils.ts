174b68fae2678878b14be2ee892b84930269e79e
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-03-13 11:24:41 +0530

    update env config

--- packages/create-gator-app/templates/nextjs-starter/src/utils/pimlicoUtils.ts
+++ packages/create-gator-app/templates/nextjs-starter/src/utils/pimlicoUtils.ts
@@ -5,6 +5,6 @@ import { http } from "viem";
 
 export const pimlicoClient = createPimlicoClient({
   transport: http(
-    `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`
+    `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`
   ),
 });
--- packages/create-gator-app/templates/nextjs-starter/src/utils/viemUtils.ts
+++ packages/create-gator-app/templates/nextjs-starter/src/utils/viemUtils.ts
@@ -8,12 +8,12 @@ import {
 
 export const bundlerClient = createBundlerClient({
   transport: http(
-    `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`
+    `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`
   ),
 });
 
 export const paymasterClient = createPaymasterClient({
   transport: http(
-    `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`
+    `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`
   ),
 });
