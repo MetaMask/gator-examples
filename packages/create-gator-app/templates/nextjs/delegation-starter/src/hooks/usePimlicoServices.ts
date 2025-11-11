3d7c3c646a67900f07bac460983bd5df8c539197
AyushBherwani1998 <ayush.bherwani@consensys.net>
2025-11-11 14:21:21 +0530

    update template to smart accounts kit

--- packages/create-gator-app/templates/nextjs/delegation-starter/src/hooks/usePimlicoServices.ts
+++ packages/create-gator-app/templates/nextjs/delegation-starter/src/hooks/usePimlicoServices.ts
@@ -2,7 +2,7 @@ import {
   createPimlicoClient,
   PimlicoClient,
 } from "permissionless/clients/pimlico";
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { http } from "viem";
 import {
   BundlerClient,
@@ -13,39 +13,34 @@ import {
 import { useChainId } from "wagmi";
 
 export function usePimlicoServices() {
-  const [paymasterClient, setPaymasterClient] = useState<PaymasterClient>();
-  const [bundlerClient, setBundlerClient] = useState<BundlerClient>();
-  const [pimlicoClient, setPimlicoClient] = useState<PimlicoClient>();
   const chainId = useChainId();
 
-  useEffect(() => {
+  const { bundlerClient, paymasterClient, pimlicoClient } = useMemo(() => {
     const pimlicoKey = process.env.NEXT_PUBLIC_PIMLICO_API_KEY;
 
     if (!pimlicoKey) {
       throw new Error("Pimlico API key is not set");
     }
 
-    const bundlerClient = createBundlerClient({
+    const bundlerClient: BundlerClient = createBundlerClient({
       transport: http(
         `https://api.pimlico.io/v2/${chainId}/rpc?apikey=${pimlicoKey}`
       ),
     });
 
-    const paymasterClient = createPaymasterClient({
+    const paymasterClient: PaymasterClient = createPaymasterClient({
       transport: http(
         `https://api.pimlico.io/v2/${chainId}/rpc?apikey=${pimlicoKey}`
       ),
     });
 
-    const pimlicoClient = createPimlicoClient({
+    const pimlicoClient: PimlicoClient = createPimlicoClient({
       transport: http(
         `https://api.pimlico.io/v2/${chainId}/rpc?apikey=${pimlicoKey}`
       ),
     });
 
-    setPimlicoClient(pimlicoClient);
-    setBundlerClient(bundlerClient);
-    setPaymasterClient(paymasterClient);
+    return { bundlerClient, paymasterClient, pimlicoClient };
   }, [chainId]);
 
   return { bundlerClient, paymasterClient, pimlicoClient };
